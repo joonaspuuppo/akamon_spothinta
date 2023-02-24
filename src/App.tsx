5186129e6e77b615b2432a332b4fff02c5adabb1
Puuppo Joonas Antti Juhana <joonas.a.j.puuppo@student.jyu.fi>
2023-02-24 20:47:41 +0200

    added functions for getting min, max, avg prices

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,63 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import BarGraph from './components/BarGraph'
 import PriceDisplay from './components/PriceDisplay'
+import transformPrice from './utils/price'
 
 function App() {
-  const [priceData, setPriceData] = useState([])
+
+  interface PriceDataElementType {
+    timestamp: string,
+    price: number,
+    deliveryArea: string,
+    unit: string
+  }
+
+  interface TransformedPriceDataType {
+    price: number,
+    time: string
+  }
+
+  const [priceData, setPriceData] = useState<Array<TransformedPriceDataType>>([])
   const [lowestPrice, setLowestPrice] = useState({price: 0, time: "00:00"})
   const [highestPrice, setHighestPrice] = useState({price: 0, time: "00:00"})
   const [avgPrice, setAvgPrice] = useState(0)
 
-  useEffect( () => {
+  useEffect( () => {  
+    
+    // On page load: fetch price data, extract price and time, calculate min & max & avg prices.
     fetch("./spot-data.json")
     .then(res => res.json())
     .then(data => {
-      setPriceData(data)
-      console.log(data)
+      const transformedData = handlePriceData(data)
+      setPriceData(transformedData)
+      calculateMinMaxAvg(transformedData)
     })
     .catch(error => console.log(error))
+
+    const handlePriceData = (priceData: Array<PriceDataElementType>) => {
+      const prices = priceData.map(elem => elem.price)
+      
+      // MWh to kWh, eur to snt, add tax 10 %
+      const transformedPrices = prices.map(price => transformPrice(price))
+
+      // getting HH:MM from timestamp
+      const times = priceData.map(elem => elem.timestamp.substring(11, 16))
+
+      const transformedData = transformedPrices.map((price, i) => ({price, time: times[i]}))
+      
+      return transformedData
+    }
+
+    const calculateMinMaxAvg = (priceData: Array<TransformedPriceDataType>) => {
+      // sorting price data to get min and max prices
+      const sortedPriceData = [...priceData].sort((a, b) => (a.price > b.price) ? 1 : -1)
+      setLowestPrice(sortedPriceData[0])
+      setHighestPrice(sortedPriceData[sortedPriceData.length - 1])
+
+      // calculating average price
+      const sumOfPrices = sortedPriceData.reduce((partialSum, a) => partialSum + a.price, 0)
+      setAvgPrice(parseFloat((sumOfPrices / sortedPriceData.length).toFixed(2)))
+    }
   }, [])
 
   return (
